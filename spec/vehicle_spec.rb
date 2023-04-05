1eeaaef1cb98bc6080b4ffad83ba5df2372e9d74
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-05 10:06:34 -0600

    add #num_adults method test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -36,19 +36,46 @@ RSpec.describe Vehicle do
     end
   end
 
-  # describe '#passengers' do 
-  #   it 'displays the passengers in the vehicle' do
-  #     vehicle = Vehicle.new("2001", "Honda", "Civic") 
+  describe '#passengers' do 
+    it 'shows the passengers in the vehicle' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic") 
 
-  #     charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-  #     jude = Passenger.new({"name" => "Jude", "age" => 20})    
-  #     taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+      expect(vehicle.passengers).to eq([])
+    end
+  end
 
-  #     expect(vehicle.speeding?).to be(false)
-  #     vehicle.speed
-  #     expect(vehicle.speeding?).to be(true)
-  #   end
-  # end
+
+  describe '#add_passenger(passenger)' do 
+    it 'puts a passenger in the vehicle' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic") 
+
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})    
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)    
+      vehicle.add_passenger(taylor) 
+
+      expect(vehicle.passengers).to eq([charlie,jude,taylor])
+    end
+  end
+
+  describe '#num_adults' do 
+    it 'counts the number of adult passengers in the vehicle' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic") 
+
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})    
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)    
+      vehicle.add_passenger(taylor) 
+      
+      expect(vehicle.num_adults).to eq(2)
+    end
+  end
 
 
 end
